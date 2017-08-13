464bf57121b4a6f3798852c15d3f1cf5bb130155
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-08-13 20:34:19 +0800

    like unlike

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
 	get "/jquery_4" => "pages#jquery_4"
 	get "/jquery_5" => "pages#jquery_5"
 
-  resources :posts
+  resources :posts do
+    member do
+      post "like" => "posts#like"
+      post "unlike" => "posts#unlike"
+    end
+  end
 	root "posts#index"
 end
