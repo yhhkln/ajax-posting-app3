f527ac4d990d74330e44f040606e780bd337f847
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-08-14 00:06:46 +0800

    ajax count like

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,8 +15,6 @@ class PostsController < ApplicationController
 	def destroy
 		@post = current_user.posts.find(params[:id])
 		@post.destroy
-
-		redirect_to posts_path
 	end
 
 	def like
@@ -24,14 +22,13 @@ class PostsController < ApplicationController
 		unless @post.find_like(current_user)
 			Like.create( :user => current_user, :post => @post )
 		end
-		redirect_to posts_path
 	end
 
 	def unlike
 		@post = Post.find(params[:id])
 		like = @post.find_like(current_user)
 		like.destroy
-		redirect_to posts_path
+		render "like"
 	end
 
 	protected
